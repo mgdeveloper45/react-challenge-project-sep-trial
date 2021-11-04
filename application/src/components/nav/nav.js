6145631a429bb1fe08e03f090738ee50aedf2498
MarkGee45 <moel55.mg@gmail.com>
2021-11-03 19:23:14 -0700

    feat(nav): add user email in src/components/nav/nav.js

--- application/src/components/nav/nav.js
+++ application/src/components/nav/nav.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { logoutUser } from '../../redux/actions/authActions' 
-import { useDispatch } from 'react-redux' 
+import { useDispatch, useSelector } from 'react-redux' 
 import { Link } from "react-router-dom";
 import "./nav.css";
 
+
 const Nav = (props) => {
+    const auth = useSelector((state) => state.auth);
     const dispatch = useDispatch()
     return (
         <div className="nav-strip">
+            <div className="nav-link-style nav-user-email">
+                <label className="nav-label">{auth.email}</label>
+            </div>
             <Link to={"/order"} className="nav-link">
                 <div className="nav-link-style">
                     <label className="nav-label">Order Form</label>
@@ -21,6 +26,7 @@ const Nav = (props) => {
             <Link to={"/"} className="nav-link">
                 <div className="nav-link-style">
                     <label className="nav-label" onClick={()=> dispatch(logoutUser())}>Log Out</label>
+                    
                 </div>
             </Link>
         </div>
